9702a47e1fe8a96367a78cbe5287174aaa1b9a03
TOMO-YOSHI <lucky.paddy10@gmail.com>
2020-08-25 03:50:07 -0700

    implemented route

--- src/reducks/store/store.js
+++ src/reducks/store/store.js
@@ -1,18 +1,24 @@
 import {
     createStore as reduxCreateStore,
     combineReducers,
+    applyMiddleware
 } from 'redux';
+import {connectRouter, routerMiddleware} from 'connected-react-router';
 
 // Import reducers
 // import { ProductsReducer } from '../products/reducers';
 import UsersReducer from '../users/reducers'
 
-const createStore = () => {
+const createStore = (history) => {
     return reduxCreateStore(
         combineReducers({
+            router: connectRouter(history),
             // products: ProductsReducer,
             users: UsersReducer,
-        })
+        }),
+        applyMiddleware(
+            routerMiddleware(history)
+        )
     );
 };
 
